1ca0151aae72c01c5733d62dc5635b15e8939d6c
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2018-03-15 01:27:36 +0100

    Video player loads, working on the video stream

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
 		pug = require('pug'),
+		fs = require('fs'),
 		bodyParser = require('body-parser'); 
 
 const server = express();
@@ -20,10 +21,16 @@ server.get('/', function(req, res) {
 	res.render("main");
 });
 
+// This get req comes from the videoplayer once it loads
+server.get('/LizzyVideo', function(req, res) {
+	res.send("./videoArchive/LizzyBeachFirstTime.mp4")
+
+})
 
 
 
 
+// Server start
 server.listen(8000, function (req, res) {
 	console.log("Server running on 8000")
 });
